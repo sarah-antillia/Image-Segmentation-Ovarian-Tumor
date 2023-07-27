797d95e1885064c7f4fc570b70914e13d54e1ac7
sarah-antillia <sarah@antillia.com>
2023-07-27 17:01:28 +0900

    Initial commit

---
+++ projects/Ovarian-Tumor/generator/ImageMaskDatasetGenerator.py
@@ -0,0 +1,181 @@
+# Copyright 2023 antillia.com Toshiyuki Arai
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ImageMaskDatasetGenerator.py
+
+import os
+import glob
+from re import A
+import shutil
+from PIL import Image, ImageOps, ImageFilter
+import cv2
+import traceback
+import numpy as np
+
+class ImageMaskDatasetGenerator:
+  
+  def __init__(self, resize=256, cropsize=256):
+    self.RESIZE   = resize
+    self.CROPSIZE = cropsize
+    self.blur_mask = True
+
+
+  def crop_image(self, image):
+    w, h = image.size
+    left  = (w - self.CROPSIZE)//2
+    upper = (h - self.CROPSIZE)//2
+    right = left  + self.CROPSIZE
+    lower = upper + self.CROPSIZE
+
+    return  image.crop( (left, upper, right, lower))
+
+  # cropped = img.crop( (left, upper, right, lower) )
+  def augment(self, image, output_dir, filename):
+    # 2023/07/27
+    #ANGLES = [30, 90, 120, 150, 180, 210, 240, 270, 300, 330]
+    ANGLES = [0, 90, 180, 270]
+    for angle in ANGLES:
+      rotated_image = image.rotate(angle)
+      output_filename = "rotated_" + str(angle) + "_" + filename
+      rotated_image_file = os.path.join(output_dir, output_filename)
+      #cropped  =  self.crop_image(rotated_image)
+      rotated_image.save(rotated_image_file)
+      print("=== Saved {}".format(rotated_image_file))
+    # Create mirrored image
+    mirrored = ImageOps.mirror(image)
+    output_filename = "mirrored_" + filename
+    image_filepath = os.path.join(output_dir, output_filename)
+    #cropped = self.crop_image(mirrored)
+    mirrored.save(image_filepath)
+    print("=== Saved {}".format(image_filepath))
+        
+    # Create flipped image
+    flipped = ImageOps.flip(image)
+    output_filename = "flipped_" + filename
+
+    image_filepath = os.path.join(output_dir, output_filename)
+    #cropped = self.crop_image(flipped)
+
+    flipped.save(image_filepath)
+    print("=== Saved {}".format(image_filepath))
+
+
+  def resize_to_square(self, image):
+     w, h  = image.size
+     pixel = image.getpixel((w-2, h-2))
+     background = Image.new("RGB", (self.RESIZE, self.RESIZE), pixel)
+     bigger = w
+     if h > bigger:
+       bigger = h
+     x = (self.RESIZE - w) // 2
+     y = (self.RESIZE - h) // 2
+     background.paste(image, (x, y))
+     return background
+  
+
+  def create(self, input_images_dir, input_masks_dir,  output_dir,
+                            debug=False):
+    output_images_dir = os.path.join(output_dir, "images")
+    output_masks_dir  = os.path.join(output_dir, "masks")
+
+    if os.path.exists(output_images_dir):
+      shutil.rmtree(output_images_dir)
+    if not os.path.exists(output_images_dir):
+      os.makedirs(output_images_dir)
+
+    if os.path.exists(output_masks_dir):
+      shutil.rmtree(output_masks_dir)
+    if not os.path.exists(output_masks_dir):
+      os.makedirs(output_masks_dir)
+
+    image_files = glob.glob(input_images_dir + "/*.JPG")
+    
+    if image_files == None or len(image_files) == 0:
+      print("FATAL ERROR: Not found mask files")
+      return
+
+    for image_file in image_files:
+      basename = os.path.basename(image_file)
+      image = Image.open(image_file).convert("RGB")
+      pngname = basename.replace(".JPG", ".PNG")
+      mask_filepath = os.path.join(input_masks_dir, pngname)
+      print("--- maskfilepath {}".format(mask_filepath))
+      mask = Image.open(mask_filepath).convert("RGB")
+      w, h = image.size
+      image = image.resize((w, h))
+      
+      basename = basename.replace(".JPG", ".jpg")
+      image_output_filepath = os.path.join(output_images_dir, basename)
+      
+      squared_image = self.resize_to_square(image)
+      # Save the cropped_square_image
+      #cropped = self.crop_image(squared_image)
+      squared_image.save(image_output_filepath)
+      print("--- Saved cropped_square_image {}".format(image_output_filepath))
+
+      self.augment(squared_image, output_images_dir, basename)
+   
+      mask = mask.resize((w, h))
+      mask_color = (255, 255, 255)
+      xmask = self.create_mono_color_mask(mask, mask_color= mask_color)
+   
+      # Blur mask 
+      if self.blur_mask:
+        print("---blurred ")
+        xmask = xmask.filter(ImageFilter.BLUR)
+      
+      if debug:
+        xmask.show()
+        input("XX")   
+      out_mask_file = image_file
+      mask_output_filepath = os.path.join(output_masks_dir, basename)
+
+      squared_mask = self.resize_to_square(xmask)
+      #cropped_mask = self.crop_image(squared_mask)
+      squared_mask.save(mask_output_filepath)
+
+      print("--- Saved cropped_squared_mask {}".format(mask_output_filepath))
+      self.augment(squared_mask, output_masks_dir, basename)
+
+
+  def create_mono_color_mask(self, mask, mask_color=(255, 255, 255)):
+    rw, rh = mask.size    
+    xmask = Image.new("RGB", (rw, rh))
+    #print("---w {} h {}".format(rw, rh))
+
+    for i in range(rw):
+      for j in range(rh):
+        color = mask.getpixel((i, j))
+        (r, g, b) = color
+        # If color is blue
+        if r>20 or g >20 or b > 20:
+          xmask.putpixel((i, j), mask_color)
+
+    return xmask
+  
+
+if __name__ == "__main__":
+  try:
+   
+    input_images_dir = "./OTU_2d/images/"
+    input_masks_dir  = "./OTU_2d/annotations/"
+    
+    generator = ImageMaskDatasetGenerator()
+    
+    output_dir = "./Ovarian-Tumor-master"
+    generator.create(input_images_dir, input_masks_dir, output_dir, debug=False)
+
+  except:
+    traceback.print_exc()
+    pass
---
+++ projects/Ovarian-Tumor/generator/split_master.py
@@ -0,0 +1,103 @@
+# Copyright 2023 antillia.com Toshiyuki Arai
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# split_master.py
+# 2023/07/28
+
+"""
+./Ovarin-Tumor-master
+ ├─images
+ └─masks
+
+"""
+# into test, train and valid dataset.
+
+"""
+Ovarin-Tumor
+├─test
+│  ├─images
+│  └─masks
+├─train
+│  ├─images
+│  └─masks
+└─valid
+  ├─images
+  └─masks
+"""
+
+
+import os
+import sys
+import glob
+import shutil
+
+import traceback
+import random
+
+def split_master(images_dir, masks_dir, output_dir):
+  image_files = glob.glob(images_dir + "/*.JPG")
+  random.shuffle(image_files)
+  num = len(image_files)
+  num_train = int(num * 0.8)
+  num_valid = int(num * 0.15)
+  num_test  = int(num * 0.05)
+  print("num_train {}".format(num_train))
+  print("num_valid {}".format(num_valid))
+  print("num_test  {}".format(num_test ))
+
+  train_files = image_files[:num_train]
+  valid_files = image_files[num_train:num_train+ num_valid]
+  test_files  = image_files[num_train+ num_valid:]
+  train_dir   = os.path.join(output_dir, "train")
+  valid_dir   = os.path.join(output_dir, "valid")
+  test_dir    = os.path.join(output_dir, "test")
+  copy(train_files, masks_dir, train_dir)
+  copy(valid_files, masks_dir, valid_dir)
+  copy(test_files,  masks_dir, test_dir )
+
+
+def copy(image_files, masks_dir, dataset_dir):
+  out_images_dir = os.path.join(dataset_dir, "images")
+  out_masks_dir  = os.path.join(dataset_dir, "masks")
+
+  if not os.path.exists(out_images_dir):
+    os.makedirs(out_images_dir)
+  if not os.path.exists(out_masks_dir):
+    os.makedirs(out_masks_dir)
+
+  for image_file in image_files:
+    shutil.copy2(image_file, out_images_dir)
+    print("Copied {} to {}".format(image_file, out_images_dir))
+
+    basename = os.path.basename(image_file)
+    mask_filepath = os.path.join(masks_dir, basename)
+    shutil.copy2(mask_filepath, out_masks_dir)
+    print("Copied {} to {}".format(mask_filepath, out_masks_dir))
+
+
+if __name__ == "__main__":
+  try:
+    images_dir = "./Ovarian-Tumor-master/images/"
+    masks_dir  = "./Ovarian-Tumor-master/masks/"
+    output_dir = "./Ovarian-Tumor/"
+    if os.path.exists(output_dir):
+      shutil.rmtree(output_dir)
+    if not os.path.exists(output_dir):
+      os.makedirs(output_dir)
+
+    split_master(images_dir, masks_dir, output_dir)
+
+  except:
+    traceback.print_exc()
+
